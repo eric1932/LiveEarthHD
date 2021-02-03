d60a02d9d3668d274d0af96118cbc5787b10edfb
Eric <eric1932777@gmail.com>
2021-02-03 01:20:32 -0600

    Increase spin speed

--- Assets/SelfRotate.cs
+++ Assets/SelfRotate.cs
@@ -10,7 +10,7 @@ public class SelfRotate : MonoBehaviour
         // transform.Rotate(new Vector3(90, 90, 90));
         // transform.rotation = Quaternion.Euler(45, 45, 45);
         Debug.Log(Time.deltaTime);
-        transform.Rotate(Vector3.up, Time.deltaTime * 5);
+        transform.Rotate(Vector3.up, Time.deltaTime * 10);
         Vector3 eulerAngles = transform.rotation.eulerAngles;
         Debug.Log("transform.rotation angles x: " + eulerAngles.x + " y: " + eulerAngles.y + " z: " + eulerAngles.z);
     }
