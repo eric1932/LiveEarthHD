ba70f85ccabddc36c4f8e6f9959f5f6c1a0ab3d7
Eric <eric1932777@gmail.com>
2021-07-13 23:59:55 +0800

    Move the Earth farther

--- Assets/NRSDK/Demos/TrackingImage/Scripts/TrackingImageExampleController.cs
+++ Assets/NRSDK/Demos/TrackingImage/Scripts/TrackingImageExampleController.cs
@@ -35,7 +35,7 @@
                 NRDebugger.Info("Create new TrackingImageVisualizer!");
                 visualizer = (TrackingImageVisualizer) Instantiate(
                     TrackingImageVisualizerPrefab,
-                    new Vector3(0, 0, 1.05f),
+                    new Vector3(0, 0, 1.6f),
                     Quaternion.identity);
                 visualizer.transform.parent = null;  // add to root of scene
 
