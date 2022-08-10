6d27243cbfa41951f60d48d50036c1ee554005ed
Eric Liu <eric1932777@gmail.com>
2022-08-09 23:11:48 -0700

    Reduce rotation speed

--- Assets/Scripts/SelfRotate.cs
+++ Assets/Scripts/SelfRotate.cs
@@ -9,7 +9,7 @@ public class SelfRotate : MonoBehaviour
     {
         // transform.Rotate(new Vector3(90, 90, 90));
         // transform.rotation = Quaternion.Euler(45, 45, 45);
-        transform.Rotate(Vector3.up, Time.deltaTime * 100);
+        transform.Rotate(Vector3.up, Time.deltaTime * 20);
         Vector3 eulerAngles = transform.rotation.eulerAngles;
     }
 }
